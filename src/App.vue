7297339cf0c270b9c91073b2b12e0b9a49dc6c7b
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-05-31 13:59:56 +0200

    Code for gameboard component and small changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import Gameboard from './components/Gameboard.vue';
 
+const gameStarted = ref(false);
+
+const startGame = () => {
+  gameStarted.value = true;
+};
 </script>
 
 <template>
-
+ <div id="app">
+    <Player @start="startGame" v-if="!gameStarted" />
+    <Gameboard v-else />
+  </div>
 </template>
 
 <style scoped>
